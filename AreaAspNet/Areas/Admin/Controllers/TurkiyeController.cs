7cd59da1c4afa0e5257520f98f51717b0146b89e
serdarunlu96 <71671958+serdarunlu96@users.noreply.github.com>
2023-08-21 13:04:23 +0300

    Add project files.

---
+++ AreaAspNet/Areas/Admin/Controllers/TurkiyeController.cs
@@ -0,0 +1,25 @@
+﻿using AreaAspNet.Areas.Admin.Data;
+using AreaAspNet.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AreaAspNet.Areas.Admin.Controllers
+{
+    public class TurkiyeController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Sehirler()
+        {
+            List<SehirViewModel> sehirler = Veri.Sehirler()
+                    .Select(x => new SehirViewModel()
+                    {
+                        Id = x.Id,
+                        SehirAd = x.SehirAd,
+                        BolgeAd = Veri.Bolgeler().First(b=>b.Id==x.BolgeId).BolgeAd,
+                        Nufus = x.Nufus
+                    })
+                    .ToList();
+
+            return View(sehirler);
+        }
+    }
+}
---
+++ AreaAspNet/Areas/Admin/Models/SehirViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AreaAspNet.Areas.Admin.Models
+{
+    public class SehirViewModel
+    {
+        public int Id { get; set; }
+        public string BolgeAd { get; set; } = null!;
+        public string SehirAd { get; set; } = null!;
+        public int Nufus { get; set; }
+    }
+}
